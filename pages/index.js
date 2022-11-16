ea46c43c27f391a0a8026b19880796b9909770d5
guy thongtrachoo <guy.t.zero@gmail.com>
2022-11-16 16:29:58 +0700

    jojo

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,24 @@ import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
+import { useEffect } from "react";
 
 export default function Home({ repositories }) {
+  useEffect(() => {
+    fetch("https://uatapi.getinvoice.net/eTax/api/process?key=U0XmmGB5VA7GFk4PbdkpniTXFH5uSW4okXeNm7N4")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+
+        }
+      );
+  },[]);
   return (
     <ContainerBlock
       title="Guy Thongtrachoo - Web Dev, App Dev, Backend"
@@ -16,12 +32,13 @@ export default function Home({ repositories }) {
       <Hero />
       <FavouriteProjects />
       <LatestCode repositories={repositories} />
+      {/* <div></div> */}
     </ContainerBlock>
   );
 }
 
 export const getServerSideProps = async () => {
-  console.log('sdsd',process.env.GITHUB_AUTH_TOKEN);
+  console.log("sdsd", process.env.GITHUB_AUTH_TOKEN);
   let token = process.env.GITHUB_AUTH_TOKEN;
 
   const repositories = await getLatestRepos(userData, token);
